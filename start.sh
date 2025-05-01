e53aedfc5e0bd9660976f50f92ca459e9d288129
svk <skapatkar@outlook.com>
2025-05-01 20:19:53 +0530

    Reorganize start.sh to improve startup sequence for Streamlit and FastAPI

--- start.sh
+++ start.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 set -e  # Fail on first error
-echo "Starting FastAPI..."
-uvicorn app.main:app --host 0.0.0.0 --port 8000 &
-
 echo "Starting Streamlit..."
 streamlit run app/ui.py --server.port 8501 --server.address 0.0.0.0
 
+sleep 5
 
-# # Start FastAPI (on port 8000) in the background
-# uvicorn app.main:app --host 0.0.0.0 --port 8000 &
-
-# # Start Streamlit (on port 8501)
-# streamlit run app/ui.py --server.port 8501
+echo "Starting FastAPI..."
+uvicorn app.main:app --host 0.0.0.0 --port 8000 &
